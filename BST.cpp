ca056209f9121d9d9e7d7f26603408a7e5072d0e
SESHUBABUM <seshu_jj@student.iitd.ac.in>
2017-06-11 12:39:34 +0530

    Create BST.cpp

---
+++ BST.cpp
@@ -0,0 +1,20 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+class BinarySearcgTree
+{
+    struct node
+    {
+        int data;
+        struct node *left;
+        struct node *right;
+    }
+    struct node *root;
+    public:
+    
+};
+int main()
+{
+ return 0;
+}
